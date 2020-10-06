0c717e50804af424c1a57eeeadc45eecba15efb1
Victor VIeira de Melo <victorvmelo@gmail.com>
2020-10-06 11:47:22 -0300

    Metodo Bolha

---
+++ Metodos de ordenacao/Bolha/bolha.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void Bolha(int V[], int n){
+    for(int i=0; i<n-1 ;i++){
+        for(int j=1; j<n-1 ;j++){
+            if(V[j]<V[j-1]){
+                int aux;
+                aux = V[j-1];
+                V[j-1] = V[j];
+                V[j] = aux;
+            }
+        }
+    }
+}
+
+int main(){
+    int vetor[10];
+
+    for(int i=0; i < 10; i++){
+        std::cin >> vetor[i];
+    }
+    for(int i=0; i < 10; i++){
+        std::cout << vetor[i] << " ";
+    }
+    std::cout << "\n";
+
+    Bolha(vetor, 10);
+
+    for(int i=0; i < 10; i++){
+        std::cout << vetor[i] << " ";
+    }
+    std::cout << "\n";
+}
